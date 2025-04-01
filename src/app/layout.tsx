a3008f45a20062c835e243b4f03e7a336e3625b7
Olushola <ememobongumoren2@gmail.com>
2025-04-01 19:12:54 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Noto_Sans } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${notoSans.variable} antialiased`}
       >
-        {children}
+          <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,84 @@
+"use client";
+
 import Image from "next/image";
-import logo from ""
+import logo from "@/public/assets/images/logo.svg";
+import sunLight from "@/public/assets/images/icon-sun.svg";
+import moonLight from "@/public/assets/images/icon-moon.svg";
+import dataExt from "@/public/data.json";
+import { Switch } from "antd";
+import { useState } from "react";
 
 export default function Home() {
+  const [filtered, setFilteredList] = useState('all');
+  const  [themeDark, setThemeDark] = useState(true)
+  const [isExtensionActive, setIsExtensionActive] = useState<null| true | false>(null)
+
+  const handleAllButton = () => {
+    setFilteredList('all')
+    setIsExtensionActive(null)
+  }
+
+  const handleActiveButton = () => {
+    setFilteredList('active')
+    setIsExtensionActive(true)
+  }
+
+  const handleInactiveButton = () => {
+    setFilteredList('inactive')
+    setIsExtensionActive(false)
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center p-14">
-      <div className="flex justify-between items-center rounded-lg">
+    <div className="flex flex-col gap-8 md:gap-14 justify-center items-center p-8 md:p-14 h-full w-full ">
+      <div className={` flex justify-between items-center rounded-2xl ${themeDark ? "bg-neutral-800"  : "bg-neutral-0"}  w-full p-4 `}>
         <div className="flex gap-5 items-center">
           <span>
-            <Image src={} alt="" />
+            <Image src={logo} alt="" />
           </span>
-          <p>Extensions</p>
         </div>
-        <div className="flex justify-center items-center">
+        <div className={` flex justify-center items-center  ${themeDark ? "bg-neutral-700"  : "bg-neutral-100"} p-2 rounded-xl cursor-pointer `} onClick={() => setThemeDark(!themeDark)}>
           <span>
-            <Image src={} alt="" />
+            <Image src={themeDark ? sunLight : moonLight} className="cursor-pointer" alt="themeIcon"  />
           </span>
         </div>
       </div>
-      <h1>Hello World</h1>
+      <div className="h-full w-full">
+        <div className="flex flex-wrap gap-5 md:gap-0 justify-center md:justify-between  items-center h-full w-full">
+          <h3 className={` text-3xl  ${themeDark ? "text-white "  : "text-neutral-900"} text-center md:text-left `}>Extensions List</h3>
+          <div className="flex gap-3 items-center">
+            <button onClick={() => handleAllButton()} className={` ${filtered==="all" ? "bg-red-500 text-neutral-900 font-bold" : `  ${themeDark ? "bg-neutral-700"  : "bg-neutral-100"} bg-neutral-700`}  py-2 px-5 cursor-pointer flex justify-center items-center rounded-full border border-neutral-500 focus:border-2 focus:border-red-500`}>
+              All
+            </button>
+            <button onClick={() => handleActiveButton()} className={` ${filtered==="active" ? "bg-red-500 text-neutral-900 font-bold" : `  ${themeDark ? "bg-neutral-700"  : "bg-neutral-100"} bg-neutral-700`} py-2 px-5 cursor-pointer flex justify-center items-center rounded-full border border-neutral-500 focus:border-2 focus:border-red-500`}>
+              Active
+            </button>
+            <button onClick={() => handleInactiveButton()} className={` ${filtered==="inactive" ? "bg-red-500 text-neutral-900 font-bold" : `  ${themeDark ? "bg-neutral-700"  : "bg-neutral-100"} bg-neutral-700`} py-2 px-5 cursor-pointer flex justify-center items-center rounded-full border border-neutral-500 focus:border-2 focus:border-red-500`}>
+              Inactive
+            </button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mt-10">
+          {dataExt.filter( (filteredItem) => filteredItem.isActive === isExtensionActive || isExtensionActive === null ).map((item) => (
+            <div className="p-5 bg-neutral-800 border border-neutral-500 flex flex-col gap-5  rounded-3xl" key={item.name}>
+              <div className="flex gap-5 h-[100px]">
+                <span>
+                  <Image src={item.logo} width={48} height={48} className="w-full h-auto object-cover" alt="icon" />
+                </span>
+                <div className="flex flex-col">
+                  <h3 className="text-white text-xl">{item.name}</h3>
+                  <p className="text-neutral-300"> {item.description} </p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <button className="py-1 px-2 cursor-pointer flex justify-center items-center rounded-full border border-neutral-500 focus:border-2 focus:border-red-500 hover:border-2 hover:font-semibold hover:border-red-500 hover:bg-red-500 hover:text-neutral-900">
+                  Remove
+                </button>
+                <Switch checked={item.isActive}  style={{ backgroundColor: item.isActive ? "#f25c54" : "#545969" }} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
